be41ebf35ccd4fc4c0fcbade0455f804e8100610
Aiden Fox-Ivey <71291879+Foxnaut@users.noreply.github.com>
2021-11-23 18:03:05 -0500

    removed _main.c

--- Source/OpenBeats/src/main.c
+++ Source/OpenBeats/src/main.c
@@ -1,56 +1,75 @@
 #include <stdbool.h> // booleans, i.e. true and false
 #include <stdio.h>   // sprintf() function
 #include <stdlib.h>  // srand() and random() functions
-
 #include "ece198.h"
 
 int main(void)
 {
-    HAL_Init(); // initialize the Hardware Abstraction Layer
-    // Peripherals (including GPIOs) are disabled by default to save power, so we
-    // use the Reset and Clock Control registers to enable the GPIO peripherals that we're using.
+    HAL_Init(); 
 
     __HAL_RCC_GPIOA_CLK_ENABLE(); // enable port A (for the on-board LED, for example)
-//    __HAL_RCC_GPIOB_CLK_ENABLE(); // enable port B (for the rotary encoder inputs, for example)
+    __HAL_RCC_GPIOB_CLK_ENABLE(); // enable port B (for the rotary encoder inputs, for example)
     __HAL_RCC_GPIOC_CLK_ENABLE(); // enable port C (for the on-board blue pushbutton, for example)
 
-    // initialize the pins to be input, output, alternate function, etc...
+    InitializePin(GPIOA, GPIO_PIN_10, GPIO_MODE_INPUT, GPIO_PULLDOWN, 0);
+    InitializePin(GPIOB, GPIO_PIN_3, GPIO_MODE_INPUT, GPIO_PULLDOWN, 0);
+    InitializePin(GPIOB, GPIO_PIN_5, GPIO_MODE_INPUT, GPIO_PULLDOWN, 0);
+    InitializePin(GPIOB, GPIO_PIN_4, GPIO_MODE_INPUT, GPIO_PULLDOWN, 0);
+    InitializePin(GPIOB, GPIO_PIN_10, GPIO_MODE_INPUT, GPIO_PULLDOWN, 0);
 
-    InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);  // on-board LED
-    // InitializePin(GPIOA, GPIO_PIN_8, GPIO_MODE_OUTPUT_PP, GPIO_PULLDOWN, 0);
-    InitializePin(GPIOC, GPIO_PIN_13, GPIO_MODE_INPUT, GPIO_PULLUP, 0);
+    // SNARE PIN
+    InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_OUTPUT_PP, GPIO_PULLDOWN, 0);
+
+    // BASS PIN
+    InitializePin(GPIOA, GPIO_PIN_6, GPIO_MODE_OUTPUT_PP, GPIO_PULLDOWN, 0);
+
     // note: the on-board pushbutton is fine with the default values (no internal pull-up resistor
     // is required, since there's one on the board)
-    
-    // set up for serial communication to the host computer
-    // (anything we write to the serial port will appear in the terminal (i.e. serial monitor) in VSCode)
+
+    // iterate through and play depending on whether high or low
+    // bool kickArray[8];    
+    // bool bassArray[8];    
+    // bool cymbolArray[8];    
+    // bool snareArray[8];    
 
     SerialSetup(9600);
     HAL_Delay(1000);
 
-    SerialPuts("This works up to here.");
+    SerialPuts("This works up to here.\n");
 
-
+    // main control flow loop up to here
     while (1) {
+
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-        HAL_Delay(50);
-        // enough that it takes some time  to load
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10)) {
 
-        if (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)) {
             while (1) {
-                HAL_Delay(50);
-                HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-                HAL_Delay(50);
-
-                if (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)) {
+                if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)) {
+                    SerialPuts("Snare selected.    \r");
+                    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 1);
+                    HAL_Delay(50);
+                    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
+                    break;
+                }
+                if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_3)) {
+                    SerialPuts("Bass selected.     \r");
+                    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
+                    HAL_Delay(50);
+                    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+                    break;
+                }
+                if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5)) {
+                    SerialPuts("Tom selected.      \r");
+                    break;
+                }
+                if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_4)) {
+                    SerialPuts("Ride selected.     \r");
                     break;
                 }
             }
-            continue;
         }
-        
     }
-
 
     return 0;
 }
@@ -60,12 +79,24 @@ void SysTick_Handler(void)
     HAL_IncTick();
 }
 
-bool delayWhilePolling( double delay, GPIO_TypeDef* buttonPort, uint8_t buttonPin ) {
-    float endTime = HAL_GetTick() + delay;
+// play this from the sorted time
+// int playLoop(int BPM, int Speaker, &bool Pattern) {
+//     // return 0 if it executes well, otherwise zero
+    
 
-    while (HAL_GetTick() < endTime) {
-        return !(HAL_GPIO_ReadPin(buttonPort, buttonPort));
-    }
+//     // read it from memory
 
-    return false;
-}
+//     // play the speaker 8 times
+
+//     return 0;
+// }
+
+// bool delayWhilePolling( double delay, GPIO_TypeDef* buttonPort, uint8_t buttonPin ) {
+//     float endTime = HAL_GetTick() + delay;
+
+//     while (HAL_GetTick() < endTime) {
+//         return !(HAL_GPIO_ReadPin(buttonPort, buttonPort));
+//     }
+
+//     return false;
+// }
