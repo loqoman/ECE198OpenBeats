cd5960e77ecaacac70f1c992119cd9ebafb6ea62
loqoman <14814137+loqoman@users.noreply.github.com>
2021-11-24 23:35:07 -0500

    :sparkles: Integraeing the pin definitions fully

--- Source/OpenBeats/src/darwinMain.c
+++ Source/OpenBeats/src/darwinMain.c
@@ -6,11 +6,11 @@
 
 // --- Pin Assignments ---
 // LEDs (temp)
-#define LED1G GPIOB, GPIO_PIN_5      // The 1st LED is beat 1 of measure one
+#define LED1G GPIOB, GPIO_PIN_5     // The 1st LED is beat 1 of measure 1
 #define LED2 GPIOB, GPIO_PIN_3
 #define LED3 GPIOB, GPIO_PIN_4 
 #define LED4 GPIOB, GPIO_PIN_10
-#define LED5G 0      // The 5th LED is beat 1 of measure two
+#define LED5G 0                     // The 5th LED is beat 1 of measure 2
 #define LED6 0 
 #define LED7 0 
 #define LED8 0 
@@ -22,7 +22,7 @@
 #define BUTTON4 GPIOC, GPIO_PIN_3
 
 // Button Multiplexer Pins
-#define MUXSIG GPIOA, GPIO_PIN_0
+#define MUXSIG GPIOB, GPIO_PIN_8
 
 #define MUXS3 GPIOB, GPIO_PIN_15
 #define MUXS2 GPIOB, GPIO_PIN_14
@@ -112,11 +112,11 @@ int main(void) {
         if (ledStates[0]) {
             HAL_GPIO_WritePin(LED1G, outputBitmask & 0x01);     // B5  -> First LED (Green)
         } if (ledStates[1]) {
-            HAL_GPIO_WritePin(LED2, outputBitmask & 0x02);     // B3  -> Second LED (Red)
+            HAL_GPIO_WritePin(LED2, outputBitmask & 0x02);      // B3  -> Second LED (Red)
         } if (ledStates[2]) {
-            HAL_GPIO_WritePin(LED3, outputBitmask & 0x04);     // B4  -> Third LED (Red) 
+            HAL_GPIO_WritePin(LED3, outputBitmask & 0x04);      // B4  -> Third LED (Red) 
         } if (ledStates[3]) {
-            HAL_GPIO_WritePin(LED4, outputBitmask & 0x08);    // B10 -> Fourth LED (Red)
+            HAL_GPIO_WritePin(LED4, outputBitmask & 0x08);      // B10 -> Fourth LED (Red) 
         }
 
         HAL_Delay(timeDelay);  // NOTE: This is the line that correponds to the BPM between beats
@@ -125,10 +125,9 @@ int main(void) {
         if(outputBitmask > 8) {  // TODO this gets changed to 8 bits with 8 LEDs
             outputBitmask = 0x1; 
         } else {
-            outputBitmask << 1;  // Shift our output bitmask by 1
+            outputBitmask *= 2;  // Shift the output bitmask
         }
         // --- Multiplexer Polling ---
-        /* UNDER CONSTRUCTION
         for(char channel = 0; channel < 5; channel++) {        // For each of the 16 channels 
                                                                 // In reality only going to check 8 channels
             // TODO: Can put a for loop here  
@@ -138,16 +137,16 @@ int main(void) {
             HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, 0);//channel & 0x08);    // S3  
             
             //uint16_t result = ReadADC(&adcInstance, ADC_CHANNEL_0);     // Read our ADC
-            uint16_t result = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+            uint16_t result = HAL_GPIO_ReadPin(MUXSIG);
 
             char buff[100];
             sprintf(buff, "Channel%d: %d\r\n",channel, result);  // %hu == "unsigned short" (16 bit)
             SerialPuts(buff);
         }
-        */
+        
         // --- (temp) Button Polling ---
         // TODO: Re-Match Button defines with proper LED's in hard (rn buttons don't match with right led)
-
+        /*
         bool button1 = HAL_GPIO_ReadPin(BUTTON1);
         bool button2 = HAL_GPIO_ReadPin(BUTTON2);
         bool button3 = HAL_GPIO_ReadPin(BUTTON3);
@@ -156,7 +155,8 @@ int main(void) {
         if(button1) {ledStates[0] ^= button1;}
         if(button2) {ledStates[1] ^= button2;}
         if(button3) {ledStates[2] ^= button3;}
-        if(button4) {ledStates[3] ^= button4;}
+        if(button4) {ledStates[3] ^= button4;} 
+        */
     } 
     return 0;
 }
